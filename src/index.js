50eff16e878718776bd6080b20168b7776e306ec
NanatsuYorou <79986479+NanatsuYorou@users.noreply.github.com>
2023-01-23 17:08:23 +0500

    Исправление путей для шрифтов

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./fonts/Proxima Nova/proximanovacond_regular.ttf";
-import "./fonts/Proxima Nova/proximanovacond_bold.ttf";
+import "./fonts/proximanovacond_regular.ttf";
+import "./fonts/proximanovacond_bold.ttf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
